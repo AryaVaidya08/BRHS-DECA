0f33d2ff53f6a7ee7645e7b7d7af3a651f3ed04d
Arya Vaidya <aryav663@gmail.com>
2025-10-20 00:16:09 -0400

    fixed sitemap

---
+++ app/sitemap.js
@@ -0,0 +1,30 @@
+export default function sitemap() {
+  const baseUrl = 'https://brhs-deca.vercel.app';
+  
+  return [
+    {
+      url: baseUrl,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1.0,
+    },
+    {
+      url: `${baseUrl}/gallery`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: `${baseUrl}/our-team`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: `${baseUrl}/resources`,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.9,
+    },
+  ];
+}
